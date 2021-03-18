c87ad86b93baa18bf4dbd98547a900655ecf2069
Sam Eriksen <sam.eriksen@bristol.ac.uk>
2021-03-18 17:04:22 +0000

    Use default GCC for now and go down to the same geant version used by LZ #5

--- Dockerfile
+++ Dockerfile
@@ -64,12 +64,9 @@ COPY scripts/bashrc_customisation.sh /home/${DOCKER_USER}/.bashrc_customisation.
 RUN cd && echo "source ${HOME}/.bashrc_customisation.sh" >> .bashrc
 
 COPY scripts/opticks_externals_installer.sh /home/${DOCKER_USER}
-COPY scripts/gcc_installer.sh /home/${DOCKER_USER}
 COPY scripts/geant4_installer.sh /home/${DOCKER_USER}
 RUN cd /home/${DOCKER_USER} \
     && export OPTICKS_EXTERNALS=/home/${DOCKER_USER}/opticks_externals \
-    && bash opticks_externals_installer.sh \
-    && cd ${HOME} && bash gcc_installer.sh \
-    && cd ${HOME} && bash geant4_installer.sh
+    && bash opticks_externals_installer.sh
 
 
--- scripts/opticks_externals_installer.sh
+++ scripts/opticks_externals_installer.sh
@@ -93,3 +93,25 @@ cd xerces-c-${xerces_version}
 ./configure --prefix=${dir}/xerces-c-${xerces_version}-install
 sudo make install
 
+
+g4_version=geant4.10.03.p01
+dir=${OPTICKS_EXTERNALS}/g4
+mkdir -p ${dir}
+cd ${dir}
+url=http://cern.ch/geant4-data/releases/${g4_version}.tar.gz
+curl -L -O $url
+tar zxf ${g4_version}.tar.gz
+mkdir ${g4_version}-build
+cd ${g4_version}-build
+cmake -G "Unix Makefiles" \
+      -DCMAKE_BUILD_TYPE=Debug \
+      -DGEANT4_INSTALL_DATA=ON \
+      -DGEANT4_USE_GDML=ON \
+      -DGEANT4_USE_SYSTEM_CLHEP=ON \
+      -DCLHEP_ROOT_DIR=${OPTICKS_EXTERNALS}/clhep/clhep_${clhep_version}-install \
+      -DGEANT4_INSTALL_DATA_TIMEOUT=3000 \
+      -DXERCESC_ROOT_DIR=${OPTICKS_EXTERNALS}/xerces/xerces-c-${xerces_version}-install \
+      -DCMAKE_INSTALL_PREFIX=${dir}/${g4_version}-install \
+      ${dir}/${g4_version}
+make -j 10
+make install
